b0eab91ce3499ea5b49af7f3a3d6d88c37f0bda4
“SuleRas” <sm.ras@hotmailco.uk>
2022-08-03 21:03:25 +0100

    Am-Pm hourDisplay commit

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,16 +1,31 @@
 $(document).ready(function () {
   //
   for (let i = 9; i < 19; i++) {
+    let hourDisplay = "";
+    if (i < 12) {
+      hourDisplay = i + "am";
+    } else if (i === 12) {
+      hourDisplay = i + "pm";
+    } else {
+      hourDisplay = i - 12 + "pm";
+    }
+
     let rowDiv = $("<div>").addClass("row time-block").attr("id", i);
-    let hourDiv = $("<div>").addClass("col-2 hour").text(i);
-    let textArea = $("<textarea>").addClass("col-8 description");
+    let hourDiv = $("<div>").addClass("col-2 hour").text(hourDisplay);
+    let textArea = $("<textarea>")
+      .addClass("col-8 description")
+      .val(localStorage.getItem(i));
     let button = $("<button>")
       .addClass("col-2 saveBtn")
       .attr("id", i)
       .text("save")
       .on("click", function () {
-        console.log(this);
+        let hourKey = $(this).attr("id");
+        let activity = $(this).siblings(".description").val();
+        localStorage.setItem(hourKey, activity);
       });
     $(".container").append(rowDiv.append(hourDiv, textArea, button));
   }
 });
+
+//change color
