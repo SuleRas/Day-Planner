a9b9ddc5326387d5533a76196254028316cce1dc
“SuleRas” <sm.ras@hotmailco.uk>
2022-08-30 21:25:30 +0100

    Final changes

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
   //
   for (let i = 9; i < 19; i++) {
+    let currentTime = moment().hour();
+
+    let rowTime = i;
+    let currentDisplay = "";
+    if (currentTime > rowTime) {
+      //past
+      currentDisplay = "past";
+    } else if (currentTime === rowTime) {
+      //present
+      currentDisplay = "present";
+    } else {
+      //future
+      currentDisplay = "future";
+    }
+
     let hourDisplay = "";
     if (i < 12) {
       hourDisplay = i + "am";
@@ -13,7 +28,7 @@ $(document).ready(function () {
     let rowDiv = $("<div>").addClass("row time-block").attr("id", i);
     let hourDiv = $("<div>").addClass("col-2 hour").text(hourDisplay);
     let textArea = $("<textarea>")
-      .addClass("col-8 description")
+      .addClass("col-8 description " + currentDisplay)
       .val(localStorage.getItem(i));
     let button = $("<button>")
       .addClass("col-2 saveBtn")
@@ -27,5 +42,3 @@ $(document).ready(function () {
     $(".container").append(rowDiv.append(hourDiv, textArea, button));
   }
 });
-
-//change color
